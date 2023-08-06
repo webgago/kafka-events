c1a81bd2323532489221d24cb2f5e5c497027c91
Anton <asozontov@gmail.com>
2023-08-06 04:17:39 +0300

    :cop:

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -11,9 +11,9 @@ SimpleCov.coverage_dir "coverage"
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::LcovFormatter,
-  SimpleCov::Formatter::JSONFormatter
-])
+                                                                 SimpleCov::Formatter::HTMLFormatter,
+                                                                 SimpleCov::Formatter::LcovFormatter,
+                                                                 SimpleCov::Formatter::JSONFormatter
+                                                               ])
 
 SimpleCov.start
